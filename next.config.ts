a0d9b8cf1df57c3b59d91ae63253a9fc7492c441
Darshit Lagdhir <darshitlagdhir@gmail.com>
2026-03-04 18:50:43 +0530

    PORT

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: '/portfolio.github.io',
+  assetPrefix: '/portfolio.github.io/',
   images: {
     unoptimized: true,
   },
